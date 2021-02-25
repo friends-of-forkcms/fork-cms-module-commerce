e77216cb582f8e384dd6019716c574e01e3b91da
Jesse Dobbelaere <jesse@dobbelae.re>
2021-02-25 20:45:25 +0100

    Add product specifications fixtures

--- src/Backend/Modules/Commerce/DataFixtures/DemoStoreFixtures.php
+++ src/Backend/Modules/Commerce/DataFixtures/DemoStoreFixtures.php
@@ -2,22 +2,23 @@
 
 namespace Backend\Modules\Commerce\DataFixtures;
 
-use Backend\Core\Language\Locale;
 use Backend\Modules\Commerce\Domain\Brand\Command\CreateBrand;
 use Backend\Modules\Commerce\Domain\Category\Command\CreateCategory;
 use Backend\Modules\Commerce\Domain\Category\Image as CategoryImage;
 use Backend\Modules\Commerce\Domain\Brand\Image as BrandImage;
 use Backend\Modules\Commerce\Domain\Country\Command\CreateCountry;
 use Backend\Modules\Commerce\Domain\OrderStatus\Command\CreateOrderStatus;
-use Backend\Modules\Commerce\Domain\PaymentMethod\PaymentMethod;
 use Backend\Modules\Commerce\Domain\Product\Command\CreateProduct;
+use Backend\Modules\Commerce\Domain\Product\Command\UpdateProduct;
 use Backend\Modules\Commerce\Domain\Product\Product;
 use Backend\Modules\Commerce\Domain\ProductOption\Command\CreateProductOption;
+use Backend\Modules\Commerce\Domain\ProductOption\ProductOption;
 use Backend\Modules\Commerce\Domain\ProductOptionValue\Command\CreateProductOptionValue;
 use Backend\Modules\Commerce\Domain\ProductSpecial\ProductSpecial;
+use Backend\Modules\Commerce\Domain\Specification\Command\CreateSpecification;
+use Backend\Modules\Commerce\Domain\SpecificationValue\Command\CreateSpecificationValue;
 use Backend\Modules\Commerce\Domain\StockStatus\Command\CreateStockStatus;
 use Backend\Modules\Commerce\Domain\Vat\Command\CreateVat;
-use Backend\Modules\Commerce\PaymentMethods\Mollie\MollieDataTransferObject;
 use Backend\Modules\MediaLibrary\Domain\MediaFolder\MediaFolder;
 use Backend\Modules\MediaLibrary\Domain\MediaGroup\Command\SaveMediaGroup;
 use Backend\Modules\MediaLibrary\Domain\MediaItem\Command\CreateMediaItemFromLocalStorageType;
@@ -46,6 +47,9 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
         'commerce_product_option_values',
         'commerce_product_options',
         'commerce_product_specials',
+        'commerce_products_specification_values',
+        'commerce_specifications',
+        'commerce_specification_values',
         'MediaGroup',
         'MediaGroupMediaItem',
         'MediaItem',
@@ -86,7 +90,8 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             $this->commandBus->handle($createCategory);
 
             // Save reference for other fixtures
-            $this->addReference(md5("category_{$createCategory->title}"), $createCategory->getCategoryEntity());
+            $referenceKey = "category_" . strtolower($createCategory->title);
+            $this->addReference(md5($referenceKey), $createCategory->getCategoryEntity());
         }
     }
 
@@ -100,7 +105,8 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             $this->commandBus->handle($createBrand);
 
             // Save reference for other fixtures
-            $this->addReference(md5("brand_{$createBrand->title}"), $createBrand->getBrandEntity());
+            $referenceKey = "brand_" . strtolower($createBrand->title);
+            $this->addReference(md5($referenceKey), $createBrand->getBrandEntity());
         }
     }
 
@@ -113,7 +119,8 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             $this->commandBus->handle($createVat);
 
             // Save reference for other fixtures
-            $this->addReference(md5("vat_{$createVat->title}"), $createVat->getVatEntity());
+            $referenceKey = "vat_" . strtolower($createVat->title);
+            $this->addReference(md5($referenceKey), $createVat->getVatEntity());
         }
     }
 
@@ -123,7 +130,8 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
         $this->commandBus->handle($createStockStatus);
 
         // Save reference for other fixtures
-        $this->addReference(md5("stock_status_{$createStockStatus->title}"), $createStockStatus->getStockStatusEntity());
+        $referenceKey = "stock_status_" . strtolower($createStockStatus->title);
+        $this->addReference(md5($referenceKey), $createStockStatus->getStockStatusEntity());
     }
 
     private function createOrderStatuses(): void {
@@ -134,12 +142,13 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             $this->commandBus->handle($createOrderStatus);
 
             // Save reference for other fixtures
-            $this->addReference(md5("order_status_{$createOrderStatus->title}"), $createOrderStatus->getOrderStatusEntity());
+            $referenceKey = "order_status_" . strtolower($createOrderStatus->title);
+            $this->addReference(md5($referenceKey), $createOrderStatus->getOrderStatusEntity());
         }
     }
 
     private function createPaymentMethods(): void {
-        $paymentMethod = new PaymentMethod("Mollie", Locale::workingLocale());
+        // $paymentMethod = new PaymentMethod("Mollie", Locale::workingLocale());
     }
 
     private function createCountries(): void {
@@ -153,7 +162,29 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
     }
 
     private function createSpecifications(): void {
+        $specifications = ['Color' => ['blue', 'orange', 'green', 'white', 'gray', 'brown', 'black', 'beige']];
 
+        foreach ($specifications as $title => $values) {
+            $createSpecification = new CreateSpecification();
+            $createSpecification->title = $title;
+            $createSpecification->filter = true;
+            $createSpecification->meta = new Meta($title, false, $title, false, $title, false, Uri::getUrl($title), false, null, SEOFollow::none(), SEOIndex::none());
+            $this->commandBus->handle($createSpecification);
+
+            $sequence = 1;
+            foreach ($values as $value) {
+                $createSpecificationValue = new CreateSpecificationValue();
+                $createSpecificationValue->value = $value;
+                $createSpecificationValue->specification = $createSpecification->getSpecificationEntity();
+                $createSpecificationValue->meta = new Meta($value, false, $value, false, $value, false, Uri::getUrl($value), false, null, SEOFollow::none(), SEOIndex::none());
+                $createSpecificationValue->sequence = $sequence++;
+                $this->commandBus->handle($createSpecificationValue);
+
+                // Save reference for other fixtures
+                $referenceKey = "specification_" . strtolower($title) . "_" . strtolower($value);
+                $this->addReference(md5($referenceKey), $createSpecificationValue->getSpecificationValueEntity());
+            }
+        }
     }
 
     private function createProducts(MediaFolder $defaultFolder): void {
@@ -169,10 +200,10 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             $createProduct->summary = $product['summary'];
             $createProduct->text = $product['text'];
             $createProduct->meta = new Meta($product['title'], false, $product['title'], false, $product['title'], false, Uri::getUrl($product['title']), false, null, SEOFollow::none(), SEOIndex::none());
-            $createProduct->category = $this->getReference(md5("category_{$product['category']}"));
-            $createProduct->brand = $this->getReference(md5("brand_{$product['brand']}"));
-            $createProduct->vat = $this->getReference(md5("vat_{$product['vat']}"));
-            $createProduct->stock_status = $this->getReference(md5("stock_status_Available"));
+            $createProduct->category = $this->getReference(md5("category_" . strtolower($product['category'])));
+            $createProduct->brand = $this->getReference(md5("brand_" . strtolower($product['brand'])));
+            $createProduct->vat = $this->getReference(md5("vat_" . strtolower($product['vat'])));
+            $createProduct->stock_status = $this->getReference(md5("stock_status_available"));
 
             if (!empty($product['offer'])) {
                 $productSpecial = new ProductSpecial();
@@ -201,12 +232,12 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
             // Create the product
             $this->commandBus->handle($createProduct);
 
-            // Add product options
-            $this->createProductOptions($createProduct->getProductEntity(), $product['options'] ?? '');
+            // Add product options and specs
+            $this->createProductOptionsAndSpecs($createProduct->getProductEntity(), $product['options'] ?? '');
         }
     }
 
-    private function createProductOptions(Product $product, string $optionsString): void {
+    private function createProductOptionsAndSpecs(Product $product, string $optionsString): void {
         if (empty($optionsString)) {
             return;
         }
@@ -223,9 +254,11 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
                 $this->commandBus->handle($createProductOption);
 
                 // Save reference for other fixtures
-                $this->addReference(md5("product_option_{$createProductOption->title}"), $createProductOption->getProductOptionEntity());
+                $referenceKey = "product_option_" . strtolower($createProductOption->title);
+                $this->addReference(md5($referenceKey), $createProductOption->getProductOptionEntity());
             }
 
+            /** @var ProductOption $productOption */
             $productOption = $this->getReference(md5("product_option_$optionName"));
 
             foreach ($optionValues as $value) {
@@ -235,6 +268,14 @@ class DemoStoreFixtures extends BaseFixture implements FixtureGroupInterface
                 $createProductOptionValue->vat = $product->getVat();
                 $createProductOptionValue->productOption = $productOption;
                 $this->commandBus->handle($createProductOptionValue);
+
+                // Also add the option as a specification
+                $specificationReferenceKey = "specification_" . strtolower($optionName) . "_" . strtolower($value);
+                if ($this->hasReference(md5($specificationReferenceKey))) {
+                    $updateProduct = new UpdateProduct($product);
+                    $updateProduct->addSpecificationValue($this->getReference(md5($specificationReferenceKey)));
+                    $this->commandBus->handle($updateProduct);
+                }
             }
         }
     }
