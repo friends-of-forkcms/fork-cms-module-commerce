2693ddbb463436a719f7b4f126560161bb786747
Jesse Dobbelaere <jesse@dobbelae.re>
2021-03-05 22:44:13 +0100

    Remove custom tabs code and introduce Alpine.js

--- src/Frontend/Themes/CommerceDemo/Core/Js/app.ts
+++ src/Frontend/Themes/CommerceDemo/Core/Js/app.ts
@@ -1,12 +1,12 @@
 // Styles
 import '../Layout/Css/app.css';
 
+// Enable AlpineJS, a minimal framework for adding "just enough" JS behavior to our HTML.
+import 'alpinejs';
+
 // Components
-import tabs from './components/tabs';
 
 document.addEventListener('DOMContentLoaded', () => {
-    tabs();
-
     // Dynamic imports with code splitting for lazy loading
     // Lazy, or "on demand", loading is a great way to optimize your site or application. This practice essentially involves splitting your
     // code at logical breakpoints, and then loading it once the user has done something that requires, or will require, a new block of code.
