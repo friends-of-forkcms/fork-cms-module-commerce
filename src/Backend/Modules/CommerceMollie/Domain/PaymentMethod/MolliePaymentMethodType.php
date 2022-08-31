dd7291151d86181a3afe7e40bfcd8a63cc0ddaf0
alessandrocraeye <alessandrocraeye@users.noreply.github.com>
2022-08-31 08:09:48 +0000

    🎨 Apply code style rules

--- src/Backend/Modules/CommerceMollie/Domain/PaymentMethod/MolliePaymentMethodType.php
+++ src/Backend/Modules/CommerceMollie/Domain/PaymentMethod/MolliePaymentMethodType.php
@@ -11,7 +11,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormFactory;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MolliePaymentMethodType extends PaymentMethodType
